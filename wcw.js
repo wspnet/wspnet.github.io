6d45eafe3e7256e4cd23f804889cd01166ae9cb9
魏水平 <wspnet@hotmail.com>
2017-05-10 22:50:17 +0800

    Create wcw.js

---
+++ wcw.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?23ff5f9d7ead38466d7da0c861e325d2";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
