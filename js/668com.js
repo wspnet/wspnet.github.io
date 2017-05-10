bef75fdc99d55d4ccf9be2789503c4bcd9734816
魏水平 <wspnet@hotmail.com>
2017-05-10 22:45:30 +0800

    Create 668com.js

---
+++ js/668com.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?9119f9f996a342acc2d1596eb97b4488";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
